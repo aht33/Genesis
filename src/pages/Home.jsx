8715c2b16aa12a1e1951304203e7eefbd3da6f84
aht33 <alexhtang03@gmail.com>
2023-11-17 09:02:33 -0600

    init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import {useEffect, useState} from 'react'
+import "../styles/Home.css"
+
+const Home = () => {
+  return (
+    <div className="header-container">Home</div>
+  )
+}
+
+export default Home
